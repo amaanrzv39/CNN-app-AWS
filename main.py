04cce5dacea7f8a52b645f9481f564781d5bf89b
amaanrzv39 <amaan.rizvi39@gmail.com>
2024-06-05 13:09:23 +0530

    added 2 stages

---
+++ main.py
@@ -0,0 +1,25 @@
+from ChickenDisease import logger
+from src.ChickenDisease.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.ChickenDisease.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
