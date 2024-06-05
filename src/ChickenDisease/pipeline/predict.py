d5deb36224f3fbfeae7a442f98dc4fbbf433ff10
amaanrzv39 <amaan.rizvi39@gmail.com>
2024-06-05 19:52:15 +0530

    CICD workflow

--- src/ChickenDisease/pipeline/predict.py
+++ src/ChickenDisease/pipeline/predict.py
@@ -20,7 +20,7 @@ class PredictionPipeline:
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        #print(result)
 
         if result[0] == 1:
             prediction = 'Healthy'
